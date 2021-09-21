0e6754faddf50a093e9b476bcd9ba1d1d7ed881c
Roger Santos <rogerioantoniodossantos@outlook.com>
2021-09-20 21:54:30 -0300

    Check user exists

--- src/index.js
+++ src/index.js
@@ -10,7 +10,16 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(400).json({ error: "User not found." })
+  }
+
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
